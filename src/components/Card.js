df3591c8704a0cdbe081c629bd752f7250179bfc
Sina Barakchi <sabvaysaft@gmail.com>
2022-07-08 16:28:20 +0430

    Searchbar value is usable and added clickcounter to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Card.module.css'
 // import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleArrowDown, faCircleArrowUp } from '@fortawesome/free-solid-svg-icons'
+// import {  } from '@fortawesome/free-solid-svg-icons';
 
-
-const Card = (props) => {
-    
+class Card extends Component {
+    constructor(){
+        super();
+        this.state ={
+            clicknum : 0,
+        }
+    }
+    UpOne = () =>{
+        this.setState(prevState => ({
+            clicknum : prevState.clicknum + 1,})
+        )
+    }
+    DownOne = () =>{
+        if (this.state.clicknum)
+        this.setState(prevState => ({
+            clicknum : prevState.clicknum - 1,
+        }))
+    }
+    render(){
+        const {clicknum} = this.state;
+        const {module,pic,prize}=this.props;
         return (
             <div className={styles.Card}>
-                <div className={styles.imgcontainer}><img src={props.pic} alt='phone_image'/></div>
+                <div className={styles.imgcontainer}><img src={pic} alt='phone_image'/></div>
                 <div className={styles.cardinfo}>
-                    <h3>{props.module}</h3>
-                    <p>{props.prize}</p>
+                    <h3>{module}</h3>
+                    <p>{prize}</p>
+                </div>
+                <div className={styles.counter}>
+                <FontAwesomeIcon icon={faCircleArrowUp} className = {styles.uparrow} onClick={this.UpOne}/>
+                
+                <p className={styles.clickcounter}>{clicknum}</p>
+                
+                <FontAwesomeIcon icon={faCircleArrowDown} className = {clicknum ? styles.downarrow : styles.deactive}  onClick={this.DownOne}/>
                 </div>
             </div>
         );
+    }
     }
 
 export default Card;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Searchbar.module.css';
 
 
-const Searchbar = () => {
+class Searchbar extends Component{
+    constructor(){
+        super();
+        this.state={
+            value : null,
+        }
 
+    }
+    ValueDetector = (event) => {
+        this.setState ({
+            value : event.target.value,
+        })
+    }
+render(){
     return(
         <div className={styles.searchbar}>
             <h1 className={styles.searchcontent}>Search What You Want</h1>
             <div className={styles.inputcontainer}>
-                <input type = 'text' className={styles.input} placeholder='Search...' />
+                <input type = 'text' className={styles.input} placeholder='Search...' onChange={this.ValueDetector}/>
+                {/* {console.log(this.state.value)} */}
             </div>
-
-
         </div>
     )
-}
+}}
 export default Searchbar;
